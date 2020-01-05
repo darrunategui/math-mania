e7ba0f8ea8ea7d081c6e48bb6a51eeaced839172
David Arrunategui <davidarrunategui@gmail.com>
2020-01-04 21:30:07 -0500

    UI improvements when finishing the game

--- src/app/game/store/reducer.ts
+++ src/app/game/store/reducer.ts
@@ -1,6 +1,6 @@
-import { createReducer, Action, on } from '@ngrx/store';
 import { GameStatus } from '@mathmania/model';
-import { answerQuestion, setDifficulty, setEllapsedTime, setQuestions, startGame, resetGame, endGame, setStatus } from './actions';
+import { Action, createReducer, on } from '@ngrx/store';
+import { endGame, resetGame, setDifficulty, setEllapsedTime, setQuestions, setStatus, startGame } from './actions';
 import { initialState, State } from './state';
 
 const gameReducer = createReducer(
@@ -10,7 +10,7 @@ const gameReducer = createReducer(
     on(setQuestions, (state, { questionsLeft, nextQuestion }) => ({ ...state, questionsQueue: questionsLeft, question: nextQuestion })),
     on(setStatus, (state, { status }) => ({ ...state, status })),
     on(startGame, (state) => ({ ...state, status: GameStatus.InProgress })),
-    on(endGame, (state) => ({ ...state, status: GameStatus.Done })),
+    on(endGame, (state) => ({ ...state, question: undefined, questionsQueue: [], status: GameStatus.Done })),
     on(resetGame, () => (initialState)),
   );
 
